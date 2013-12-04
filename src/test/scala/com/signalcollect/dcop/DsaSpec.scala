e4a30fc6f1f7d67600ef3a3324b94dbdff01bb17
Philip Stutz <philip.stutz@gmail.com>
2013-12-04 13:51:19 +0100

    Renamed package and added Apache 2 header.

--- src/test/scala/com/signalcollect/dcop/DsaSpec.scala
+++ src/test/scala/com/signalcollect/dcop/DsaSpec.scala
@@ -1,13 +1,32 @@
-package optimizers
+/*
+ *  @author Philip Stutz
+ *  @author Mihaela Verman
+ *  
+ *  Copyright 2013 University of Zurich
+ *      
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
 
+package com.signalcollect.dcop
+
+import scala.util.Random
 import org.scalatest.FlatSpec
 import org.scalatest.ShouldMatchers
 import org.scalatest.prop.Checkers
-import com.signalcollect._
-import scala.util.Random
-import org.scalacheck.Gen
-import org.scalacheck.Gen._
-import org.scalacheck.Arbitrary
+import com.signalcollect.GraphBuilder
+import com.signalcollect.IdStateMapAggregator
+import com.signalcollect.StateForwarderEdge
 
 class DsaSpec extends FlatSpec with ShouldMatchers with Checkers {
 
