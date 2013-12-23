615a00a4e05ced358efcf6d58c96b97d6649d1f0
Ela Verman <elaverman@gmail.com>
2013-12-23 14:30:59 +0200

    Turned DcopAlgorithmRun from App to case class.
    Added a Grid case class, removed RankedDcopAlgorithmRun, now everything
    is treated based on the optimizer type in DcopAlgorithmRun.

--- src/main/scala/com/signalcollect/dcop/DcopAlgorithmRun.scala
+++ src/main/scala/com/signalcollect/dcop/DcopAlgorithmRun.scala
@@ -9,7 +9,7 @@ import com.signalcollect._
 import com.signalcollect.configuration.ExecutionMode
 import com.signalcollect.interfaces.ModularAggregationOperation
 
-case class ColorPrinter(valuesInLine: Int) {
+case class ColorPrinter(grid: Grid) {
   def shouldTerminate(f: java.io.FileWriter, g: java.io.FileWriter)(aggregate: Map[Int, Int]): Boolean = {
     println("Currently in printer object")
     val sorted = aggregate.toList.sortBy(x => x._1)
@@ -17,7 +17,7 @@ case class ColorPrinter(valuesInLine: Int) {
       case (id, color) =>
         f.write(color.toString)
         //print(color.toString)
-        if ((id + 1) % valuesInLine == 0) {
+        if ((id + 1) % grid.valuesInLine == 0) {
           f.write("\n")
           //println
         } else {
@@ -29,7 +29,7 @@ case class ColorPrinter(valuesInLine: Int) {
     println("****")
     val numberOfConflicts = aggregate.map {
       case (id, color) =>
-        val neighbors = DcopAlgorithmRun.neighbours(id, valuesInLine)
+        val neighbors = grid.computeNeighbours(id)
         val conflictsForId = neighbors.map(aggregate(_)).filter(_ == color)
         conflictsForId.size
     }.sum / 2
@@ -45,11 +45,11 @@ class ColorPrintingGlobalTerminationCondition(
   startTime: Long,
   gridWidth: Int,
   aggregationOperation: IdStateMapAggregator[Int, Int],
-  aggregationInterval: Long) extends GlobalTerminationCondition[Map[Int, Int]](aggregationOperation, aggregationInterval, ColorPrinter(gridWidth).shouldTerminate(f, g))
+  aggregationInterval: Long,
+  grid: Grid) extends GlobalTerminationCondition[Map[Int, Int]](aggregationOperation, aggregationInterval, ColorPrinter(grid).shouldTerminate(f, g))
   with Serializable
 
-object DcopAlgorithmRun extends App {
-
+case class Grid(valuesInLine: Int) {
   // Returns all the neighboring cells of the cell with the given row/column
   def potentialNeighbours(column: Int, row: Int): List[(Int, Int)] = {
     List(
@@ -59,42 +59,53 @@ object DcopAlgorithmRun extends App {
   }
 
   // Tests if a cell is within the grid boundaries
-  def inGrid(column: Int, row: Int, width: Int): Boolean = {
-    column >= 0 && row >= 0 && column < width && row < width
+  def inGrid(column: Int, row: Int): Boolean = {
+    column >= 0 && row >= 0 && column < valuesInLine && row < valuesInLine
   }
 
-  def neighbours(id: Int, width: Int): Iterable[Int] = {
-
-    val column: Int = id % width
-    val row: Int = id / width
-
-    potentialNeighbours(column, row).filter(coordinate => inGrid(coordinate._1, coordinate._2, width)) map
-      (coordinate => (coordinate._2 * width + coordinate._1))
+  def computeNeighbours(id: Int): Iterable[Int] = {
+    val column: Int = id % valuesInLine
+    val row: Int = id / valuesInLine
+    potentialNeighbours(column, row).filter(coordinate => inGrid(coordinate._1, coordinate._2)) map
+      (coordinate => (coordinate._2 * valuesInLine + coordinate._1))
   }
+}
+
+case class GridDcopAlgorithmRun(optimizer: DcopAlgorithm[Int, Int], domain: Set[Int], width: Int, runNumber: Int) {
 
   println("Starting.")
   val g = GraphBuilder.build
-  //  val optimizer = DsaAVertexColoring(changeProbability = 1.0)
-  //  val optimizer = DsaBVertexColoring(changeProbability = 1.0)
-  val optimizer = ConflictDsaBVertexColoring(changeProbability = 1.0)
-  val domain = (0 to 3).toSet
-  val width = 100
+
   def randomFromDomain = domain.toSeq(Random.nextInt(domain.size))
   val debug = false
 
-  for (i <- 0 until width * width)
-    g.addVertex(new SimpleDcopVertex(i, domain, optimizer, randomFromDomain, debug = debug))
+  val grid = Grid(width)
 
-  for (i <- 0 until width * width)
-    for (n <- neighbours(i, width))
-      g.addEdge(i, new StateForwarderEdge(n))
-
+  optimizer match {
+    
+    case rankedOptimizer: OptimizerModule[Int, Int] with RankedConfiguration[Int, Int] =>
+      println("Ranked Optimizer")
+      for (i <- 0 until width * width)
+        g.addVertex(new RankedDcopVertex(i, domain, rankedOptimizer, randomFromDomain, debug = debug))
+      for (i <- 0 until width * width)
+        for (n <- grid.computeNeighbours(i))
+          g.addEdge(i, new RankedVertexColoringEdge(n))
+          
+    case simpleOptimizer: OptimizerModule[Int, Int] =>
+      println("Simple Optimizer")
+      for (i <- 0 until width * width)
+        g.addVertex(new SimpleDcopVertex(i, domain, simpleOptimizer, randomFromDomain, debug = debug))
+      for (i <- 0 until width * width)
+        for (n <- grid.computeNeighbours(i))
+          g.addEdge(i, new StateForwarderEdge(n))
+  }
   println("Preparing Execution configuration.")
 
-  val out = new java.io.FileWriter("animation.txt")
-  val outConflicts = new java.io.FileWriter("resultsConflicts.txt")
+  //TODO: modify output file names
+  val out = new java.io.FileWriter(s"animation$runNumber.txt")
+  val outConflicts = new java.io.FileWriter(s"conflicts$runNumber.txt")
   var startTime = System.nanoTime()
-  val terminationCondition = new ColorPrintingGlobalTerminationCondition(out, outConflicts, startTime, width, aggregationOperation = new IdStateMapAggregator[Int, Int], aggregationInterval = 100L)
+  val terminationCondition = new ColorPrintingGlobalTerminationCondition(out, outConflicts, startTime, width, aggregationOperation = new IdStateMapAggregator[Int, Int], aggregationInterval = 100L, grid = grid)
 
   val executionConfigSync = ExecutionConfiguration(ExecutionMode.PureAsynchronous).withSignalThreshold(0.01).withGlobalTerminationCondition(terminationCondition).withTimeLimit(100000L) //(420000)
 
@@ -102,7 +113,7 @@ object DcopAlgorithmRun extends App {
   g.execute(executionConfigSync)
   println("Shutting down.")
   g.shutdown
- 
+
   out.close
   outConflicts.close
 
