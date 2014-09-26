fbf945a17ea0f007bc1e6900a574417ff828bd12
Ela Verman <elaverman@gmail.com>
2014-09-26 15:49:08 +0200

    Graph initialized inside every test iteration.
    Leads to lower running times. Note: Initial tests take longer, probably
    due to the JVM warming-up.

--- src/test/scala/com/signalcollect/dcop/ModulesSpec.scala
+++ src/test/scala/com/signalcollect/dcop/ModulesSpec.scala
@@ -32,59 +32,91 @@ import com.signalcollect.dcop.evaluation._
 
 class ModulesSpec extends FlatSpec with ShouldMatchers with Checkers with TestAnnouncements {
 
-  "A 2-Vertex graph" should "correctly assign 2-colors" in {
+
+  "A 2-Vertex Ranked-graph" should "correctly assign 2-colors" in {
     check(
       {
         def initial0Value = 0
-        val g = GraphBuilder.build
-        for (i <- (1 to 50)){
-          val vertex0 = new SimpleDcopVertex(0, Set(0, 1), new SimpleOptimizer[Int, Int](0.5), initial0Value, debug = false)
-          val vertex1 = new SimpleDcopVertex(1, Set(0, 1), new SimpleOptimizer[Int, Int](0.5), initial0Value, debug = false)
-          g.addVertex(vertex0)
-          g.addVertex(vertex1)
-          g.addEdge(0, new StateForwarderEdge(1))
-          g.addEdge(1, new StateForwarderEdge(0))
-          val executionConfiguration = new ExecutionConfiguration
-          println(g.execute(executionConfiguration))
-          assert(vertex0.state != vertex1.state, "Color collision")
-          g.reset
+
+        for (i <- (1 to 50)) {
+          val g = GraphBuilder.build
+          try {
+            val vertex0 = new RankedDcopVertex(0, Set(0, 1), new RankedDsaAVertexColoringOptimizer[Int, Int](0.5), initial0Value, debug = false)
+            val vertex1 = new RankedDcopVertex(1, Set(0, 1), new RankedDsaAVertexColoringOptimizer[Int, Int](0.5), initial0Value, debug = false)
+            g.addVertex(vertex0)
+            g.addVertex(vertex1)
+            g.addEdge(0, new RankedVertexColoringEdge(1))
+            g.addEdge(1, new RankedVertexColoringEdge(0))
+            val executionConfiguration = new ExecutionConfiguration
+            println(g.execute(executionConfiguration))
+            assert(vertex0.state != vertex1.state, "Color collision")
+            assert(math.abs(vertex0.currentConfig.ranks(vertex0.id) - vertex1.currentConfig.ranks(vertex1.id)) < 0.01, "Should have almost the same rank for both vertices.")
+            assert(math.abs(vertex0.currentConfig.ranks(vertex0.id) - vertex1.currentConfig.ranks(vertex0.id)) < 0.01, "Should have the correct rank of the neighbour")
+          } finally {
+            g.shutdown
+          }
         }
-        g.shutdown
         true
       },
       minSuccessful(1))
-  }
-  
-    "A 2-Vertex Ranked-graph" should "correctly assign 2-colors" in {
-    check(
-      {
-        def initial0Value = 0
-        val g = GraphBuilder.build
-        for (i <- (1 to 50)){
-          
-          val vertex0 = new RankedDcopVertex(0, Set(0, 1), new RankedDsaAVertexColoringOptimizer[Int, Int](0.5), initial0Value, debug = false)
-          val vertex1 = new RankedDcopVertex(1, Set(0, 1), new RankedDsaAVertexColoringOptimizer[Int, Int](0.5), initial0Value, debug = false)
-          g.addVertex(vertex0)
-          g.addVertex(vertex1)
-          g.addEdge(0, new RankedVertexColoringEdge(1))
-          g.addEdge(1, new RankedVertexColoringEdge(0))
-          val executionConfiguration = new ExecutionConfiguration
-          println(g.execute(executionConfiguration))
-          assert(vertex0.state != vertex1.state, "Color collision")
-          assert(math.abs(vertex0.currentConfig.ranks(vertex0.id)-vertex1.currentConfig.ranks(vertex1.id)) < 0.01, "Should have almost the same rank for both vertices.")
-          assert(math.abs(vertex0.currentConfig.ranks(vertex0.id)-vertex1.currentConfig.ranks(vertex0.id)) < 0.01, "Should have the correct rank of the neighbour")
-          g.reset
-        }
-        g.shutdown
-        true
-      },
-      minSuccessful(1))
-  }
-    
-  it should "have all vertices in a local minimum" in {
-    
   }
 
+  it should "have all vertices in a local minimum" in {
+    check(
+      {
+        def initial0Value = 0
+
+        for (i <- (1 to 50)) {
+          val g = GraphBuilder.build
+          try {
+            val vertex0 = new RankedDcopVertex(0, Set(0, 1), new RankedDsaAVertexColoringOptimizer[Int, Int](0.5), initial0Value, debug = false)
+            val vertex1 = new RankedDcopVertex(1, Set(0, 1), new RankedDsaAVertexColoringOptimizer[Int, Int](0.5), initial0Value, debug = false)
+            g.addVertex(vertex0)
+            g.addVertex(vertex1)
+            g.addEdge(0, new RankedVertexColoringEdge(1))
+            g.addEdge(1, new RankedVertexColoringEdge(0))
+            val executionConfiguration = new ExecutionConfiguration
+            println(g.execute(executionConfiguration))
+            assert(vertex0.state != vertex1.state, "Color collision")
+            assert(math.abs(vertex0.currentConfig.ranks(vertex0.id) - vertex1.currentConfig.ranks(vertex1.id)) < 0.01, "Should have almost the same rank for both vertices.")
+            assert(math.abs(vertex0.currentConfig.ranks(vertex0.id) - vertex1.currentConfig.ranks(vertex0.id)) < 0.01, "Should have the correct rank of the neighbour")
+          } finally {
+            g.shutdown
+          }
+        }
+        true
+      },
+      minSuccessful(1))
+  }
+
+    "A 2-Vertex graph" should "correctly assign 2-colors" in {
+    check(
+      {
+        def initial0Value = 0
+
+        for (i <- (1 to 50)) {
+          val g = GraphBuilder.build
+          try {
+            val vertex0 = new SimpleDcopVertex(0, Set(0, 1), new SimpleOptimizer[Int, Int](0.5), initial0Value, debug = false)
+            val vertex1 = new SimpleDcopVertex(1, Set(0, 1), new SimpleOptimizer[Int, Int](0.5), initial0Value, debug = false)
+            g.addVertex(vertex0)
+            g.addVertex(vertex1)
+            g.addEdge(0, new StateForwarderEdge(1))
+            g.addEdge(1, new StateForwarderEdge(0))
+            val executionConfiguration = new ExecutionConfiguration
+            println(g.execute(executionConfiguration))
+            assert(vertex0.state != vertex1.state, "Color collision")
+          } finally {
+            g.shutdown
+          }
+        }
+
+        true
+      },
+      minSuccessful(1))
+  }
+
+  
 }
 
 
