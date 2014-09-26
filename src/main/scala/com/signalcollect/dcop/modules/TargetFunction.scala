44691f7b3b29c2edd522310e8c47e73adb878699
Ela Verman <elaverman@gmail.com>
2014-09-27 00:32:26 +0200

    Added diagrams.

--- src/main/scala/com/signalcollect/dcop/modules/TargetFunction.scala
+++ src/main/scala/com/signalcollect/dcop/modules/TargetFunction.scala
@@ -1,5 +1,6 @@
 package com.signalcollect.dcop.modules
 
+//TODO Config <: Configuration[AgentId, Action]?
 trait TargetFunction[AgentId, Action, Config, UtilityType] extends Serializable {
   def computeExpectedUtilities(c: Config): Map[Action, UtilityType]
 }
