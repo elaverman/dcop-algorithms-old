2a1b42df39964efa37cc59c3e1e45a67f1e3d5f7
Ela Verman <elaverman@gmail.com>
2014-10-07 15:05:35 +0200

    Changed executionConfiguration usage in test.
    To comply with updated signal-collect.

--- src/test/scala/com/signalcollect/dcop/ModulesSpec.scala
+++ src/test/scala/com/signalcollect/dcop/ModulesSpec.scala
@@ -47,8 +47,7 @@ class ModulesSpec extends FlatSpec with ShouldMatchers with Checkers with TestAn
             g.addVertex(vertex1)
             g.addEdge(0, new RankedVertexColoringEdge(1))
             g.addEdge(1, new RankedVertexColoringEdge(0))
-            val executionConfiguration = new ExecutionConfiguration
-            println(g.execute(executionConfiguration))
+            println(g.execute)
             assert(vertex0.state != vertex1.state, "Color collision")
             assert(math.abs(vertex0.currentConfig.ranks(vertex0.id) - vertex1.currentConfig.ranks(vertex1.id)) < 0.01, "Should have almost the same rank for both vertices.")
             assert(math.abs(vertex0.currentConfig.ranks(vertex0.id) - vertex1.currentConfig.ranks(vertex0.id)) < 0.01, "Should have the correct rank of the neighbour")
@@ -75,8 +74,7 @@ class ModulesSpec extends FlatSpec with ShouldMatchers with Checkers with TestAn
             g.addVertex(vertex1)
             g.addEdge(0, new RankedVertexColoringEdge(1))
             g.addEdge(1, new RankedVertexColoringEdge(0))
-            val executionConfiguration = new ExecutionConfiguration
-            println(g.execute(executionConfiguration))
+            println(g.execute)
             assert(vertex0.state != vertex1.state, "Color collision")
             assert(math.abs(vertex0.currentConfig.ranks(vertex0.id) - vertex1.currentConfig.ranks(vertex1.id)) < 0.01, "Should have almost the same rank for both vertices.")
             assert(math.abs(vertex0.currentConfig.ranks(vertex0.id) - vertex1.currentConfig.ranks(vertex0.id)) < 0.01, "Should have the correct rank of the neighbour")
@@ -103,8 +101,33 @@ class ModulesSpec extends FlatSpec with ShouldMatchers with Checkers with TestAn
             g.addVertex(vertex1)
             g.addEdge(0, new StateForwarderEdge(1))
             g.addEdge(1, new StateForwarderEdge(0))
-            val executionConfiguration = new ExecutionConfiguration
-            println(g.execute(executionConfiguration))
+            println(g.execute)
+            assert(vertex0.state != vertex1.state, "Color collision")
+          } finally {
+            g.shutdown
+          }
+        }
+
+        true
+      },
+      minSuccessful(1))
+  }
+    
+    "A 2-Vertex graph with memory" should "correctly assign 2-colors" in {
+    check(
+      {
+        def initial0Value = 0
+
+        for (i <- (1 to 50)) {
+          val g = GraphBuilder.build
+          try {
+            val vertex0 = new SimpleDcopVertex(0, Set(0, 1), new SimpleOptimizer[Int, Int](0.5), initial0Value, debug = false)
+            val vertex1 = new SimpleDcopVertex(1, Set(0, 1), new SimpleOptimizer[Int, Int](0.5), initial0Value, debug = false)
+            g.addVertex(vertex0)
+            g.addVertex(vertex1)
+            g.addEdge(0, new StateForwarderEdge(1))
+            g.addEdge(1, new StateForwarderEdge(0))
+            println(g.execute)
             assert(vertex0.state != vertex1.state, "Color collision")
           } finally {
             g.shutdown
