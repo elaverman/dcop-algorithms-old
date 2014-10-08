40bec71676032401331ad2bf2c70ccc6b63b0264
Ela Verman <elaverman@gmail.com>
2014-10-08 14:54:54 +0200

    Moved graph structure tests and evaluation into a separate project.

--- src/main/scala/com/signalcollect/dcop/evaluation/package.scala
+++
@@ -1,6 +0,0 @@
-package com.signalcollect.dcop
-
-package object evaluation {
-  type RowType = Tuple21[String, String, Double, Int, Int, String, Int, Double, Double, Int, Int, Double, String, Int, Int, String, String, String, Double, String, String]
-//  type ResultType = Tuple21[String, String, Double, Int, Int, String, Int, Double, Double, Int, Int, Long, String, Int, Int, String, String, String, Double, String, String]		  
-}
