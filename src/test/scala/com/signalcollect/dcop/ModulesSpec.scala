422f6562ec0a06484b2da009e80f9e4eba2f47aa
Ela Verman <elaverman@gmail.com>
2014-10-09 16:12:22 +0200

    Repaired ModulesSpec.

--- src/test/scala/com/signalcollect/dcop/ModulesSpec.scala
+++ src/test/scala/com/signalcollect/dcop/ModulesSpec.scala
@@ -29,7 +29,7 @@ import com.signalcollect.dcop.graph._
 import org.scalacheck.Gen
 import org.scalacheck.Arbitrary
 import com.signalcollect.dcop.optimizers._
-import com.signalcollect.dcop.impl.RankedConfig
+import com.signalcollect.dcop.impl._
 
 class ModulesSpec extends FlatSpec with ShouldMatchers with Checkers with TestAnnouncements {
 
@@ -67,110 +67,138 @@ class ModulesSpec extends FlatSpec with ShouldMatchers with Checkers with TestAn
       minSuccessful(1))
   }
 
-//  it should "have all vertices in a local minimum" in {
-//    check(
-//      {
-//        def initial0Value = 0
-//
-//        for (i <- (1 to 50)) {
-//          val g = GraphBuilder.build
-//          try {
-//            val vertex0 = new RankedDcopVertex(0, Set(0, 1), new RankedDsaAVertexColoring[Int, Int](0.5), initial0Value, debug = false)
-//            val vertex1 = new RankedDcopVertex(1, Set(0, 1), new RankedDsaAVertexColoring[Int, Int](0.5), initial0Value, debug = false)
-//            g.addVertex(vertex0)
-//            g.addVertex(vertex1)
-//            g.addEdge(0, new RankedVertexColoringEdge(1))
-//            g.addEdge(1, new RankedVertexColoringEdge(0))
-//            println(g.execute)
-//            assert(vertex0.state._1 != vertex1.state._1, "Color collision")
-//            assert(math.abs(vertex0.currentConfig.ranks(vertex0.id) - vertex1.currentConfig.ranks(vertex1.id)) < 0.01, "Should have almost the same rank for both vertices.")
-//            assert(math.abs(vertex0.currentConfig.ranks(vertex0.id) - vertex1.currentConfig.ranks(vertex0.id)) < 0.01, "Should have the correct rank of the neighbour")
-//          } finally {
-//            g.shutdown
-//          }
-//        }
-//        true
-//      },
-//      minSuccessful(1))
-//  }
-//
-//  "A 2-Vertex graph with memory running Fading Memory JSFP-I" should "correctly assign 2-colors" in {
-//    check(
-//      {
-//        def initial0Value = 0
-//
-//        for (i <- (1 to 50)) {
-//          val g = GraphBuilder.build
-//          try {
-//            val vertex0 = new MemoryDcopVertex(0, Set(0, 1), new FadingMemoryJsfpiVertexColoring[Int, Int](0.5, 0.7), initial0Value, debug = false)
-//            val vertex1 = new MemoryDcopVertex(1, Set(0, 1), new FadingMemoryJsfpiVertexColoring[Int, Int](0.5, 0.7), initial0Value, debug = false)
-//            g.addVertex(vertex0)
-//            g.addVertex(vertex1)
-//            g.addEdge(0, new MemoryVertexColoringEdge(1))
-//            g.addEdge(1, new MemoryVertexColoringEdge(0))
-//            println(g.execute)
-//            assert(vertex0.state._1 != vertex1.state._1, "Color collision")
-//          } finally {
-//            g.shutdown
-//          }
-//        }
-//
-//        true
-//      },
-//      minSuccessful(1))
-//  }
-//
-//  "A 2-Vertex graph with memory running WRM-I" should "correctly assign 2-colors" in {
-//    check(
-//      {
-//        def initial0Value = 0
-//
-//        for (i <- (1 to 50)) {
-//          val g = GraphBuilder.build
-//          try {
-//            val vertex0 = new MemoryDcopVertex(0, Set(0, 1), new WrmiVertexColoring[Int, Int](0.5, 0.7, 0.5), initial0Value, debug = false)
-//            val vertex1 = new MemoryDcopVertex(1, Set(0, 1), new WrmiVertexColoring[Int, Int](0.5, 0.7, 0.5), initial0Value, debug = false)
-//            g.addVertex(vertex0)
-//            g.addVertex(vertex1)
-//            g.addEdge(0, new MemoryVertexColoringEdge(1))
-//            g.addEdge(1, new MemoryVertexColoringEdge(0))
-//            println(g.execute)
-//            assert(vertex0.state._1 != vertex1.state._1, "Color collision")
-//          } finally {
-//            g.shutdown
-//          }
-//        }
-//
-//        true
-//      },
-//      minSuccessful(1))
-//  }
-//
-//  "A 2-Vertex graph" should "correctly assign 2-colors" in {
-//    check(
-//      {
-//        def initial0Value = 0
-//
-//        for (i <- (1 to 50)) {
-//          val g = GraphBuilder.build
-//          try {
-//            val vertex0 = new SimpleDcopVertex(0, Set(0, 1), new DsaAVertexColoring[Int, Int](0.5), initial0Value, debug = false)
-//            val vertex1 = new SimpleDcopVertex(1, Set(0, 1), new DsaAVertexColoring[Int, Int](0.5), initial0Value, debug = false)
-//            g.addVertex(vertex0)
-//            g.addVertex(vertex1)
-//            g.addEdge(0, new StateForwarderEdge(1))
-//            g.addEdge(1, new StateForwarderEdge(0))
-//            println(g.execute)
-//            assert(vertex0.state != vertex1.state, "Color collision")
-//          } finally {
-//            g.shutdown
-//          }
-//        }
-//
-//        true
-//      },
-//      minSuccessful(1))
-//  }
+  it should "have all vertices in a local minimum" in {
+    check(
+      {
+        def initial0Value = 0
+
+        for (i <- (1 to 50)) {
+          val g = GraphBuilder.build
+          try {
+            def initialConf(id: Int) = RankedConfig(
+              neighborhood = Map.empty[Int, Int].withDefaultValue(0),
+              numberOfCollects = 0,
+              ranks = Map.empty[Int, Double].withDefaultValue(0),
+              domain = Set(0, 1),
+              centralVariableAssignment = (id, initial0Value))
+
+            val vertex0 = new RankedDcopVertex(new RankedDsaAVertexColoring[Int, Int](0.5), initialConf(0), debug = false)
+            val vertex1 = new RankedDcopVertex(new RankedDsaAVertexColoring[Int, Int](0.5), initialConf(1), debug = false)
+            g.addVertex(vertex0)
+            g.addVertex(vertex1)
+            g.addEdge(0, new RankedVertexColoringEdge(1))
+            g.addEdge(1, new RankedVertexColoringEdge(0))
+            println(g.execute)
+            assert(vertex0.state.centralVariableAssignment != vertex1.state.centralVariableAssignment, "Color collision")
+            assert(math.abs(vertex0.currentConfig.ranks(vertex0.id) - vertex1.currentConfig.ranks(vertex1.id)) < 0.01, "Should have almost the same rank for both vertices.")
+            assert(math.abs(vertex0.currentConfig.ranks(vertex0.id) - vertex1.currentConfig.ranks(vertex0.id)) < 0.01, "Should have the correct rank of the neighbour")
+          } finally {
+            g.shutdown
+          }
+        }
+        true
+      },
+      minSuccessful(1))
+  }
+
+  "A 2-Vertex graph with memory running Fading Memory JSFP-I" should "correctly assign 2-colors" in {
+    check(
+      {
+        def initial0Value = 0
+
+        for (i <- (1 to 50)) {
+          val g = GraphBuilder.build
+          try {
+            def initialConf(id: Int) = SimpleMemoryConfig[Int, Int, Double](
+              neighborhood = Map.empty[Int, Int].withDefaultValue(0),
+              memory = Map.empty[Int, Double].withDefaultValue(0),
+              numberOfCollects = 0,
+              domain = Set(0, 1),
+              centralVariableAssignment = (id, initial0Value))
+
+            val vertex0 = new MemoryDcopVertex(new FadingMemoryJsfpiVertexColoring[Int, Int](0.5, 0.7), initialConf(0), debug = false)
+            val vertex1 = new MemoryDcopVertex(new FadingMemoryJsfpiVertexColoring[Int, Int](0.5, 0.7), initialConf(1), debug = false)
+            g.addVertex(vertex0)
+            g.addVertex(vertex1)
+            g.addEdge(0, new MemoryVertexColoringEdge(1))
+            g.addEdge(1, new MemoryVertexColoringEdge(0))
+            println(g.execute)
+            assert(vertex0.state.centralVariableAssignment != vertex1.state.centralVariableAssignment, "Color collision")
+          } finally {
+            g.shutdown
+          }
+        }
+
+        true
+      },
+      minSuccessful(1))
+  }
+
+  "A 2-Vertex graph with memory running WRM-I" should "correctly assign 2-colors" in {
+    check(
+      {
+        def initial0Value = 0
+
+        for (i <- (1 to 50)) {
+          val g = GraphBuilder.build
+          try {
+
+            def initialConf(id: Int) = SimpleMemoryConfig[Int, Int, Double](
+              neighborhood = Map.empty[Int, Int].withDefaultValue(0),
+              memory = Map.empty[Int, Double].withDefaultValue(0),
+              numberOfCollects = 0,
+              domain = Set(0, 1),
+              centralVariableAssignment = (id, initial0Value))
+
+            val vertex0 = new MemoryDcopVertex(new WrmiVertexColoring[Int, Int](0.5, 0.7, 0.5), initialConf(0), debug = false)
+            val vertex1 = new MemoryDcopVertex(new WrmiVertexColoring[Int, Int](0.5, 0.7, 0.5), initialConf(1), debug = false)
+            g.addVertex(vertex0)
+            g.addVertex(vertex1)
+            g.addEdge(0, new MemoryVertexColoringEdge(1))
+            g.addEdge(1, new MemoryVertexColoringEdge(0))
+            println(g.execute)
+            assert(vertex0.state.centralVariableAssignment != vertex1.state.centralVariableAssignment, "Color collision")
+          } finally {
+            g.shutdown
+          }
+        }
+
+        true
+      },
+      minSuccessful(1))
+  }
+
+  "A 2-Vertex graph" should "correctly assign 2-colors" in {
+    check(
+      {
+        def initial0Value = 0
+
+        for (i <- (1 to 50)) {
+          val g = GraphBuilder.build
+          try {
+            def initialConf(id: Int) = SimpleConfig[Int, Int](
+              neighborhood = Map.empty[Int, Int].withDefaultValue(0),
+              numberOfCollects = 0,
+              domain = Set(0, 1),
+              centralVariableAssignment = (id, initial0Value))
+
+            val vertex0 = new SimpleDcopVertex(new DsaAVertexColoring[Int, Int](0.5), initialConf(0), debug = false)
+            val vertex1 = new SimpleDcopVertex(new DsaAVertexColoring[Int, Int](0.5), initialConf(1), debug = false)
+            g.addVertex(vertex0)
+            g.addVertex(vertex1)
+            g.addEdge(0, new StateForwarderEdge(1))
+            g.addEdge(1, new StateForwarderEdge(0))
+            println(g.execute)
+            assert(vertex0.state.centralVariableAssignment != vertex1.state.centralVariableAssignment, "Color collision")
+          } finally {
+            g.shutdown
+          }
+        }
+
+        true
+      },
+      minSuccessful(1))
+  }
 
 }
 
