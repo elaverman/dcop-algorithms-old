1c000a61dfea736790a402401ca1d86a30cb3c85
Ela Verman <elaverman@gmail.com>
2014-10-11 17:16:11 +0200

    Added Serializable.

--- src/main/scala/com/signalcollect/dcop/modules/Optimizer.scala
+++ src/main/scala/com/signalcollect/dcop/modules/Optimizer.scala
@@ -1,6 +1,6 @@
 package com.signalcollect.dcop.modules
 
-trait Optimizer[AgentId, Action, Config <: Configuration[AgentId, Action], UtilityType] {
+trait Optimizer[AgentId, Action, Config <: Configuration[AgentId, Action], UtilityType] extends Serializable {
   def schedule: AdjustmentSchedule[AgentId, Action, Config]
   def rule: DecisionRule[AgentId, Action, Config] with TargetFunction[AgentId, Action, Config, UtilityType] with UtilityFunction[AgentId, Action, Config, UtilityType]
 
