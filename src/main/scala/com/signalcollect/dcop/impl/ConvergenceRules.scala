331760fcad3ec0ef8c12fed580fc4e55ef582734
Ela Verman <elaverman@gmail.com>
2014-10-11 17:17:34 +0200

    Commented out some convergence rules. (Debug)

--- src/main/scala/com/signalcollect/dcop/impl/ConvergenceRules.scala
+++ src/main/scala/com/signalcollect/dcop/impl/ConvergenceRules.scala
@@ -13,40 +13,32 @@ override def shouldTerminate(c: Config): Boolean = isInLocalOptimum(c)
 }
 
 
-/**
- * Is converged only when there are no more conflicts. Not based on the target or utility function.
- */
-trait ZeroConflictConvergence[AgentId, Action, Config <: Configuration[AgentId, Action]] extends DecisionRule[AgentId, Action, Config] {
-
-  /**
-   * No delegation between isConverged and isConvergedGivenUtilitiesAndMaxUtility
-   */
-  override def shouldTerminate(c: Config): Boolean = {
-    c.computeExpectedNumberOfConflicts == 0
-  }
-}
-
-/**
- * Main use for hard constraints, where you have negative utility
- * for violating constraints and reach 0 utility only when no constraints are violated.
- * One example is: ConflictBasedVertexColoringUtility
- */
-trait ZeroUtilityConvergence[AgentId, Action, Config <: Configuration[AgentId, Action]] extends DecisionRule[AgentId, Action, Config] with TargetFunction[AgentId, Action, Config, Double] {
+///**
+// * Is converged only when there are no more conflicts. Not based on the target or utility function.
+// */
+//trait ZeroConflictConvergence[AgentId, Action, Config <: Configuration[AgentId, Action]] extends DecisionRule[AgentId, Action, Config] {
 //
-//  override protected def isInLocalOptimumGivenUtilitiesAndMaxUtility(
-//    c: Config, 
-//    expectedUtilities: Map[Action, Double], 
-//    maxUtility: Double): Boolean = {
+//  /**
+//   * No delegation between isConverged and isConvergedGivenUtilitiesAndMaxUtility
+//   */
+//  override def shouldTerminate(c: Config): Boolean = {
+//    c.computeExpectedNumberOfConflicts == 0
+//  }
+//}
+//
+///**
+// * Main use for hard constraints, where you have negative utility
+// * for violating constraints and reach 0 utility only when no constraints are violated.
+// * One example is: ConflictBasedVertexColoringUtility
+// */
+//trait ZeroUtilityConvergence[AgentId, Action, Config <: Configuration[AgentId, Action]] extends DecisionRule[AgentId, Action, Config] with TargetFunction[AgentId, Action, Config, Double] {
+//
+//  /**
+//   * No delegation between shouldTerminate and isConvergedGivenUtilitiesAndMaxUtility
+//   */
+//  override def shouldTerminate(c: Config): Boolean = {
+//    val expectedUtilities: Map[Action, Double] = computeExpectedUtilities(c)
 //    val currentUtility = expectedUtilities(c.centralVariableValue)
 //    currentUtility == 0
 //  }
-
-  /**
-   * No delegation between isConverged and isConvergedGivenUtilitiesAndMaxUtility
-   */
-  override def shouldTerminate(c: Config): Boolean = {
-    val expectedUtilities: Map[Action, Double] = computeExpectedUtilities(c)
-    val currentUtility = expectedUtilities(c.centralVariableValue)
-    currentUtility == 0
-  }
-}
+//}
