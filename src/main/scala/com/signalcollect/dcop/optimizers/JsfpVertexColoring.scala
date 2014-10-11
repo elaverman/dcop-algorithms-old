76aba0179c3fc9adc74cb0832f3d151dd7eecd5a
Ela Verman <elaverman@gmail.com>
2014-10-11 17:15:03 +0200

    Removed unnecessary params from Jsfp and Wrmi.

--- src/main/scala/com/signalcollect/dcop/optimizers/JsfpVertexColoring.scala
+++ src/main/scala/com/signalcollect/dcop/optimizers/JsfpVertexColoring.scala
@@ -1,4 +1,4 @@
-package com.signalcollect.dcop
+package com.signalcollect.dcop.optimizers
 
 import scala.util.Random
 import com.signalcollect.dcop.modules._
@@ -13,14 +13,14 @@ class FadingMemoryJsfpiVertexColoring[AgentId, Action](changeProbability: Double
     with VertexColoringUtility[AgentId, Action, SimpleMemoryConfig[AgentId, Action, Double]] { 
         def rho = rhoValue
       }
-  override def toString = "JsfpiVertexColoringChangeProbability" + changeProbability + "rhoValue" + rhoValue
+  override def toString = "FadingMemoryJsfpiVertexColoringChangeProbability" + changeProbability + "rhoValue" + rhoValue
 }
 
-class JsfpiVertexColoring[AgentId, Action](changeProbability: Double, rhoValue: Double) extends Optimizer[AgentId, Action, SimpleMemoryConfig[AgentId, Action, Double], Double] {
+class JsfpiVertexColoring[AgentId, Action](changeProbability: Double) extends Optimizer[AgentId, Action, SimpleMemoryConfig[AgentId, Action, Double], Double] {
   val schedule = new ParallelRandomAdjustmentSchedule[AgentId, Action, SimpleMemoryConfig[AgentId, Action, Double]](changeProbability)
   val rule = new ArgmaxADecisionRule[AgentId, Action, SimpleMemoryConfig[AgentId, Action, Double]] 
     with NashEquilibriumConvergence[AgentId, Action, SimpleMemoryConfig[AgentId, Action, Double]] 
     with AverageExpectedUtilityTargetFunction[AgentId, Action, SimpleMemoryConfig[AgentId, Action, Double]] 
     with VertexColoringUtility[AgentId, Action, SimpleMemoryConfig[AgentId, Action, Double]] 
-  override def toString = "JsfpiVertexColoringChangeProbability" + changeProbability + "rhoValue" + rhoValue
+  override def toString = "JsfpiVertexColoringChangeProbability" + changeProbability 
 }
